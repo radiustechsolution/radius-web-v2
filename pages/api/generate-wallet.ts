ccbff29dfb93b02a0025fb849c560f9c444f72e8
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-07 18:43:42 +0100

    updated

--- pages/api/generate-wallet.ts
+++ pages/api/generate-wallet.ts
@@ -1,5 +1,6 @@
 // pages/api/generate-wallet.ts
 import { PrismaClient } from "@prisma/client";
+import { error } from "console";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
@@ -12,59 +13,63 @@ export default async function handler(
     const { ref, first_name, last_name, email, phone_number, customer_id } =
       req.body;
 
-    // try {
-    const response = await fetch(
-      "https://appapi.radiustech.com.ng/api/virtualaccountnew",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          ref,
-          first_name,
-          last_name,
-          email,
-          phone_number,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        signal: AbortSignal.timeout(50000),
+    try {
+      //   const response = await fetch(
+      //     "https://appapi.radiustech.com.ng/api/virtualaccountnew",
+      //     {
+      //       method: "POST",
+      //       body: JSON.stringify({
+      //         ref,
+      //         first_name,
+      //         last_name,
+      //         email,
+      //         phone_number,
+      //       }),
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //         Accept: "application/json",
+      //       },
+      //       signal: AbortSignal.timeout(50000),
+      //     }
+      //   );
+
+      const account_name = "Olumide Oludowole";
+      const account_number = "2029838389";
+      const bank_name = "Wema";
+
+      if (1 == 1) {
+        // const resJson = await response.json();
+        // const { account_number, bank_name } = resJson.data;
+
+        // Store the virtual account in the database
+        await prisma.virtual_accounts.create({
+          data: {
+            customer_id: Number(customer_id),
+            account_id: ref,
+            account_reference: ref,
+            account_number,
+            account_name: `${first_name} ${last_name}`,
+            bank_name,
+            bank_code: "1234",
+          },
+        });
+
+        return res.status(200).json({
+          message: "Virtual account created successfully!",
+          data: { account_number, bank_name },
+        });
+      } else {
+        // return res.status(response.status).json({
+        //   message: "Failed to create virtual account",
+        // });
       }
-    );
-
-    if (response.ok) {
-      const resJson = await response.json();
-      const { account_number, bank_name } = resJson.data;
-
-      // Store the virtual account in the database
-      await prisma.virtual_accounts.create({
-        data: {
-          customer_id: Number(customer_id),
-          account_id: ref,
-          account_reference: ref,
-          account_number,
-          account_name: `${first_name} ${last_name}`,
-          bank_name,
-          bank_code: "1234",
-        },
-      });
-
-      return res.status(200).json({
-        message: "Virtual account created successfully!",
-        data: { account_number, bank_name },
-      });
-    } else {
-      return res.status(response.status).json({
-        message: "Failed to create virtual account",
+    } catch (error: any) {
+      console.error("Virtual account creation error:", error.message);
+      return res.status(500).json({
+        message: "Error creating virtual account",
+        error: error.message,
       });
     }
-    // } catch (error: any) {
-    //   console.error("Virtual account creation error:", error.message);
-    //   return res.status(500).json({
-    //     message: "Error creating virtual account",
-    //     error: error.message,
-    //   });
-    // }
   } else {
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
