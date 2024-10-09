c292026dc40355eb4765b7d5a1b9fbef6fffe04b
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-09 14:14:58 +0100

    updated

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPWA({ nextConfig });
+module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development",
+// });
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = withPWA({ nextConfig });
