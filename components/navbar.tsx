c1351f02951fd6e6c9f542ddbde22035ebb487ca
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-10 03:09:39 +0100

    changed landing img to an avater

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <div className="max-w-[1300px] w-full px-4">
-        {/* Log */}
+        {/* Logo */}
         <div className="flex py-5 items-center gap-2">
           <BsFillRSquareFill color={siteConfig.siteColors.primary} size={30} />
           <h1 className="text-[20px] text-black font-extrabold">RADIUS</h1>
