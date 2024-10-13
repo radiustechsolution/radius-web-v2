5b5cfc709d72b96df96b107a8596559fdaa5f7c7
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-13 23:25:38 +0100

    updated

--- pages/dashboard/airtime.tsx
+++ pages/dashboard/airtime.tsx
@@ -157,6 +157,8 @@ const AirtimePage = () => {
                               ? "AIRTEL"
                               : "undetermined"
                     );
+                  setPhoneNumber("");
+                  setAmount("");
                 }}
                 required
                 disabled={loading}
