dcfe71cd297fef6dac871f4a6f9aa51658776bd5
Oludowole Olumide <xeonncodes@gmail.com>
2024-10-19 08:06:43 +0100

    updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,13 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          key="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          name="viewport"
+        />
+      </Head>
       <body
         className={clsx(
           "bg-background font-sans antialiased",
