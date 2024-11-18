e17f87a6523924c09d140ceacafc4c111a9490e4
Oludowole Olumide <xeonncodes@gmail.com>
2024-11-18 06:51:55 +0100

    updated

--- pages/api/app/store.tsx
+++ pages/api/app/store.tsx
@@ -13,41 +13,44 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
-
-  const schema = Joi.object({
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    username: Joi.string().required(),
-    phone_number: Joi.string().required(),
-    password: Joi.string().min(6).required(),
-    promo_code: Joi.string(),
-  });
-
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
-  }
-
-  const {
-    first_name,
-    last_name,
-    email,
-    username,
-    phone_number,
-    password,
-    promo_code,
-  } = req.body;
-
-  const randomToken = randomBytes(16).toString("hex");
-  const randomPromoCode: string = String(generateSixDigitNumber());
-  const hashedToken = bcrypt.hashSync(randomToken, 10);
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
   try {
+    // Check method
+    if (req.method !== "POST") {
+      return res.status(405).json({ message: "Method not allowed" });
+    }
+
+    const schema = Joi.object({
+      first_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      username: Joi.string().required(),
+      phone_number: Joi.string().required(),
+      password: Joi.string().min(6).required(),
+      promo_code: Joi.string(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ status: 400, message: error.details[0].message });
+    }
+
+    const {
+      first_name,
+      last_name,
+      email,
+      username,
+      phone_number,
+      password,
+      promo_code,
+    } = req.body;
+
+    const randomToken = randomBytes(16).toString("hex");
+    const randomPromoCode: string = String(generateSixDigitNumber());
+    const hashedToken = bcrypt.hashSync(randomToken, 10);
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
     const [existingPhone, existingUsername, existingEmail] = await Promise.all([
       prisma.user.findUnique({ where: { phone_number } }),
       prisma.user.findUnique({ where: { username } }),
@@ -55,16 +58,23 @@ export default async function handler(
     ]);
 
     if (existingPhone) {
-      return res.status(400).json({ error: "Phone number already exists" });
+      return res
+        .status(400)
+        .json({ status: 400, message: "Phone number already exists" });
     }
     if (existingUsername) {
-      return res.status(400).json({ error: "Username already exists" });
+      return res
+        .status(400)
+        .json({ status: 400, message: "Username already exists" });
     }
     if (existingEmail) {
-      return res.status(400).json({ error: "Email already exists" });
+      return res
+        .status(400)
+        .json({ status: 400, message: "Email already exists" });
     }
 
     let invited_code = promo_code.toLowerCase() || "radius";
+
     const promocode = await prisma.user.findFirst({
       where: { username: promo_code.toLowerCase() },
     });
@@ -94,10 +104,14 @@ export default async function handler(
       "New Customer Registration"
     );
 
-    return res.status(201).json({ message: "User created successfully", user });
+    return res
+      .status(201)
+      .json({ status: 201, message: "User created successfully", data: user });
   } catch (error: any) {
     return res.status(400).json({
-      error: error.message || "An error occurred during registration.",
+      status: 400,
+      message: "Something went wrong!. Try again",
+      error: error.message || "none",
     });
   }
 }
