e81d83dcdbe897a3b762af2d33114f437fd05fc6
Oludowole Olumide <xeonncodes@gmail.com>
2024-11-27 21:18:28 +0100

    updated

--- lib/sendmail.ts
+++ lib/sendmail.ts
@@ -1,24 +1,24 @@
 // lib/api.js
 export const sendEmail = async (mail_to: any, message: any, subject: any) => {
   try {
-    // const response = await fetch(
-    //   `https://nulgen.aortadev.website/v1/sendemail?mail_to=${mail_to}&message=${message}&subject=${subject}`,
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Accept: "application/json",
-    //     },
-    //   }
-    // );
+    const response = await fetch(
+      `https://nulgen.aortadev.website/v1/sendemail?mail_to=${mail_to}&message=${message}&subject=${subject}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
 
-    // const data = await response.json();
+    const data = await response.json();
 
-    // if (!response.ok) {
-    //   throw new Error(data.message || "Something went wrong");
-    // }
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong");
+    }
 
-    return true;
+    return data;
   } catch (error) {
     console.error("Email sending failed:", error);
     throw error;
