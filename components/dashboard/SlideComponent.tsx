04f31c36b6ce36dcfb2dc522c7ad90401192b418
Oludowole Olumide <xeonncodes@gmail.com>
2024-12-18 04:18:57 +0100

    Added slier

--- components/dashboard/SlideComponent.tsx
+++ components/dashboard/SlideComponent.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 export const SlideComponent = () => {
   return (
     <Marquee delay={2} className="bg-[#DBEAFE] py-1 text-primary">
-      🎉 It's Radius' One Year Anniversary! We sincerely thank you for being a valued customer. Your satisfaction remains our top priority! 🎉
+      🎉 It&apos;s Radius&apos; One Year Anniversary! We sincerely thank you for being a valued customer. Your satisfaction remains our top priority! 🎉
     </Marquee>
   )
 }
