d0f129a19bf60371a94299e126b3c484d980ee78
Oludowole Olumide <xeonncodes@gmail.com>
2024-12-20 08:11:40 +0100

    updated

--- components/dashboard/SlideComponent.tsx
+++ components/dashboard/SlideComponent.tsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 
 export const SlideComponent = () => {
   return (
-    <Marquee delay={2} className="bg-[#DBEAFE] py-1 text-primary">
+    <Marquee delay={2} className="bg-[white] py-1 text-primary">
       Merry Christmas 🎄 and Happy New Year! 🎉 May this holiday season be
       filled with joy, peace, and love. ❤️
     </Marquee>
