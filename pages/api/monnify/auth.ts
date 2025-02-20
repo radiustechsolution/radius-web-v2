7d798fa4e2147c46a2b0cdc50cc3c7b6fd3f16f7
Oludowole Olumide <xeonncodes@gmail.com>
2025-02-20 19:53:45 +0100

    scs

--- pages/api/monnify/auth.ts
+++ pages/api/monnify/auth.ts
@@ -34,51 +34,49 @@ export default async function handler(
     const encodedCredentials = base64Encode(apiKey, clientSecret);
 
     if (adminUser) {
-      console.log("Yes adminUser");
-
       // Check if the token is older than 30 minutes
       const thirtyMinutesAgo = dayjs().subtract(30, "minutes");
 
-      if (dayjs(adminUser.created_at).isBefore(thirtyMinutesAgo)) {
-        console.log("Older");
-        // Make the POST request to Monnify to get the access token
-        const response = await fetch(
-          `${process.env.MONNIFY_BASE_URL}/api/v1/auth/login`,
-          {
-            method: "POST",
-            headers: {
-              Authorization: `Basic ${encodedCredentials}`, // Add the Base64-encoded API Key and Client Secret
-              "Content-Type": "application/json", // Set the content type
-            },
-          }
-        );
-
-        const data = await response.json();
-        console.log(data);
-        if (!response.ok) {
-          // Handle errors returned by the Monnify API
-          res.status(response.status).json({ error: data });
-          return;
+      // if (dayjs(adminUser.created_at).isBefore(thirtyMinutesAgo)) {
+      // console.log("Older");
+      // Make the POST request to Monnify to get the access token
+      const response = await fetch(
+        `${process.env.MONNIFY_BASE_URL}/api/v1/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Basic ${encodedCredentials}`, // Add the Base64-encoded API Key and Client Secret
+            "Content-Type": "application/json", // Set the content type
+          },
         }
+      );
 
-        // Update the token in the "safe" column of the admin table
-        await prisma.user.update({
-          where: {
-            id: 232, // Specify the admin ID or adjust based on your logic
-          },
-          data: {
-            pin: data.responseBody.accessToken, // token
-            created_at: new Date(), // Update created_at to the current timestamp
-          },
-        });
-
-        console.log(data);
-        // Send back the access token
-        res.status(200).json(data);
-      } else {
-        // If the token is still valid (less than 30 minutes old), send the current token
-        res.status(200).json({ access_token: adminUser.pin });
+      const data = await response.json();
+      // console.log(data);
+      if (!response.ok) {
+        // Handle errors returned by the Monnify API
+        res.status(response.status).json({ error: data });
+        return;
       }
+
+      // Update the token in the "safe" column of the admin table
+      await prisma.user.update({
+        where: {
+          id: 232, // Specify the admin ID or adjust based on your logic
+        },
+        data: {
+          pin: data.responseBody.accessToken, // token
+          created_at: new Date(), // Update created_at to the current timestamp
+        },
+      });
+
+      // console.log(data);
+      // Send back the access token
+      res.status(200).json(data);
+      // } else {
+      //   // If the token is still valid (less than 30 minutes old), send the current token
+      //   res.status(200).json({ access_token: adminUser.pin });
+      // }
     } else {
       // Handle case where admin user is not found
       res.status(404).json({ error: "Admin user not found" });
