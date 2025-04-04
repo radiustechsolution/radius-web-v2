852ce0a0122030fe0b1ca05e899ed183646aac89
Oludowole Olumide <xeonncodes@gmail.com>
2025-04-04 20:48:59 +0100

    jsbsjb

--- lib/object.ts
+++ lib/object.ts
@@ -1,7 +1,7 @@
 export const blockedEmail = [
   "xeonncodes@gmail.com",
   "abiodunoluwaseunvictor@gmail.com",
-  // "radiustechsolution@gmail.com",
+  "radiustechsolution@gmail.com",
   "elitedigitalskill@gmail.com",
   "ebizdavid2@gmail.com",
   "fayowolebenjamin99@gmail.com",
